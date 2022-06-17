374a9bd2db4c478459362706d69997eb0d6cc25c
nana <prajna270505@gmail.com>
2022-06-17 09:56:56 +0700

    20. Update Menu Aplikasi

--- ci4/app/Views/Kategori/select.php
+++ ci4/app/Views/Kategori/select.php
@@ -2,30 +2,41 @@
 
 <?= $this->section('content') ?>
 
-<a class="btn btn-primary" href="<?= base_url('/admin/kategori/create')?>" role="button">Tambah Kategori</a>
+<div class="row">
 
-<h1><?= $judul ?></h1>
+    <div class="col-4">
+        <a class="btn btn-primary" href="<?= base_url('/admin/kategori/create')?>" role="button">Tambah Kategori</a>
+    </div>
 
-<table class="table">
-    <tr>
-        <th>No</th>
-        <th>Kategori</th>
-        <th>Keterangan</th>
-        <th>Hapus</th>
-        <th>Edit</th>
-    </tr>
+    <div class="col">
+        <h3><?= $judul ?></h3>
+    </div>
+</div>
 
-    <?php $no=1?>
-    <?php foreach($kategori as $key => $value):?>
-    <tr>
-        <td><?= $no++ ?></td>
-        <td><?= $value['kategori'] ?></td>
-        <td><?= $value['keterangan']?></td>
-        <td><a href="<?= base_url()?>/admin/kategori/delete/<?= $value['idkategori']?>">Hapus</a></td>
-        <td><a href="<?= base_url()?>/admin/kategori/find/<?= $value['idkategori']?>">Edit</a></td>
-    </tr>
-    <?php endforeach;?>
-    <!-- $kategori object dari controller kategori -->
-</table>
-<?= $pager->links('group1','bootstrap') ?>
+
+<div class="row mt-2">
+
+    <table class="table">
+        <tr>
+            <th>No</th>
+            <th>Kategori</th>
+            <th>Keterangan</th>
+            <th>Hapus</th>
+            <th>Edit</th>
+        </tr>
+
+        <?php $no=1?>
+        <?php foreach($kategori as $key => $value):?>
+        <tr>
+            <td><?= $no++ ?></td>
+            <td><?= $value['kategori'] ?></td>
+            <td><?= $value['keterangan']?></td>
+            <td><a href="<?= base_url()?>/admin/kategori/delete/<?= $value['idkategori']?>">Hapus</a></td>
+            <td><a href="<?= base_url()?>/admin/kategori/find/<?= $value['idkategori']?>">Edit</a></td>
+        </tr>
+        <?php endforeach;?>
+        <!-- $kategori object dari controller kategori -->
+    </table>
+    <?= $pager->links('group1','bootstrap') ?>
+</div>
 <?= $this->endSection() ?>
