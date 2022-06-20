de8ed36e0756d0b19be514f716169fa7805c4c12
nana <prajna270505@gmail.com>
2022-06-20 11:35:15 +0700

    38.Update Rincian Order

--- ci4/app/Views/order/update.php
+++ ci4/app/Views/order/update.php
@@ -21,7 +21,55 @@
     </div>
 </div>
 
+<!-- Form update -->
 <div class="row">
+    <div class="col">
+        <form action="<?= base_url('/admin/order/update')?>" method="post">
+            
+            <div class="form-group">
+                <label for="kategori">BAYAR</label>
+                <input type="number" name="bayar" class="form-control" required>
+            </div>   
+                <input type="hidden" name="total" value="<?= $order [0]['total'] ?>" class="form-control" required>
+                <input type="hidden" name="idorder" value="<?= $order [0]['idorder'] ?>" class="form-control" required>
+            
+            <div class="form-group mt-2">
+                <button class="btn btn-success" style="width:100%" type="submit" name="simpan">Simpan</button>
+            </div>
+    </div>
+    </form>
+</div>
 
+<!-- Judul -->
+<div class="row">
+    <div class="col">
+        <h2>Rincian Order</h2>
+    </div>
+</div>
+
+<!-- table data -->
+<div class="row">
+    <div class="col">
+        <table class="table">
+            <tr>
+                <th>No</th>
+                <th>Menu</th>
+                <th>Harga Jual</th>
+                <th>Jumlah Beli</th>
+                <th>Total</th>
+            </tr>
+            <?php 
+            $no = 1;
+            foreach($detail as $value):?>
+                <tr>
+                    <td><?= $no++?></td>
+                    <td><?= $value['menu']?></td>
+                    <td><?= $value['hargajual']?></td>
+                    <td><?= $value['jumlah'] ?></td>
+                    <td><?= $value['jumlah'] * $value['hargajual']; ?></td>
+                </tr>
+            <?php endforeach;?> 
+        </table>
+    </div>
 </div>
 <?= $this->endSection() ?> 
